be57332e6e6937034ebaf56c0851d7f51abd77cc
Roman Ožana <roman@wikidi.com>
2014-05-27 10:43:34 +0200

    Keep directory structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,17 +5,18 @@ var coffee = require("gulp-coffee"),
 		less = require("gulp-less"),
 		concat = require("gulp-concat"),
 		zip = require("gulp-zip");
+path = require('path');
 
 gulp.task("zip", function () {
 	return gulp.src([
-				'css/*',
-				'img/*',
-				'js/*',
-				'manifest.json',
-				'popup.html',
-				'timer128x128.png',
-				'timer32x32.png'
-			])
+		'css/*',
+		'img/*',
+		'js/*',
+		'manifest.json',
+		'popup.html',
+		'timer128x128.png',
+		'timer32x32.png'
+	], {base: "."})
 			.pipe(zip('simple_time_track.zip'))
 			.pipe(gulp.dest('dist'));
 });
